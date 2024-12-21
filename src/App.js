ed96b89c30a5cf36d7845b8c43c047571c4a7f62
Ernest <rabergcobo@gmail.com>
2024-12-20 21:58:47 -0800

    Comments removed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-// aaatodos los prop names e escriben en camelCase
-// los atributos numericos se escriben entre llaves
-// boolean true se escribir solo con el nombre de la propiedad, {false}, con llaves. ejemplo, quiero autoFocus en true, solo pongo autoFocus
-// El atributo class se escribe como className
-//
-
 import "./css/App.css";
 import "bulma/css/bulma.css";
 import Navbar from "./components/Navbar";
